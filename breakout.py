81b32cfdf30c75aff93c4d0f1a566b3a4f6e90e3
danielnv051 <danielnv0910@gmail.com>
2023-10-18 21:28:52 +0330

    complete pong.py and breakout.py

--- breakout.py
+++ breakout.py
@@ -1,25 +1,38 @@
 import random
 import arcade
 
+
 class Ball(arcade.Sprite):
     ...
+
 
 class Stone(arcade.Sprite):
     ...
 
+
 class Rocket(arcade.Sprite):
     ...
 
+
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(width=800,height=600,title='BreakOut',center_window=True)
-        arcade.load_texture(':resources:images/backgrounds/abstract_1.jpg')
+        super().__init__(width=800, height=600, title="BreakOut", center_window=True)
+        self.background = arcade.load_texture(
+            ":resources:images/backgrounds/abstract_1.jpg"
+        )
+        self.width = 800
+        self.height = 600
+        
 
     def on_draw(self):
         arcade.start_render()
 
-        arcade.finish_render()
-        
+        arcade.draw_lrwh_rectangle_textured(
+            0, 0, self.width, self.height, self.background
+        )
 
-window = Game()
+        arcade.finish_render()
+
+
+game = Game()
 arcade.run()
